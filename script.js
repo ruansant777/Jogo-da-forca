e45d0d3f475df07db42bc78e72648a84d5e4ccc5
ruansant777 <ruan.mendes.santana@escola.pr.gov.br>
2025-09-29 10:28:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 let palavras = ["python", "desenvolvimento", "programcao", "algoritmo", "github"];
+let palavras = palavras[Math.floor(Math.random () * palavras.length)];
+let letrasCorretas = [];
+let letrasErradas = [];
+let tentativas = 6;
+
+function exibirPalavra () {
+  let exibir = palavra.split("").map(letra => letrasCorretas.includes(letra) ? letra: "__").join(" ");
+  document.getElementById("palavra").textContent = exibir;
+}
+
+function verificarLetra() {
+  const letra = document.getElementById("inputLetra").value.toLowerCase();
+
+if (letra=== " " || letra.length >1) {
+  return;
+}
+
+if (letrasCorreta.includes(letra))
