eff4da9cbab8e0c23c2daab590ee775b0fee9d27
ruansant777 <ruan.mendes.santana@escola.pr.gov.br>
2025-09-30 11:18:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -16,4 +16,25 @@ if (letra=== " " || letra.length >1) {
   return;
 }
 
-if (letrasCorreta.includes(letra))
+if (letrasCorreta.includes(letra) || letraErradas.includes(letra)) {
+  document.getElementById("mensagem").textContent = "cê ja tentou essa letra bocó";
+  return;
+}
+
+  if (palavras.includes(letra)) {
+    letrasCorretas.push(letra);
+    if (!palavra.split("").some(letra=> !letraCorretas.includes(letra))) }
+  document.getElementById("mensagem").textContent = "boa mlk! acertou!";
+}
+} else {
+  letrasErradas.push(letra);
+  tentativas--;
+  document.getElementById("mensagem").textContent = "letra errada burro";
+}
+
+document.getElementById("tentativas").textContent = tentativas;
+document.getElementById("letrasErradas").textContent = letrasErradas.join(", ");
+exibirPalavra();
+
+if (tentativas === 0) {
+  document.getElementById(",mensagem")
